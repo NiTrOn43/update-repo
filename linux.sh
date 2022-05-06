da6bc119352845b7c94f15a9dfb6a199b93e1b1e
NitrOn43 <school8gresh@gmail.com>
2022-05-06 15:41:18 +0300

    update termux and linux

---
+++ linux.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+clear
+sudo apt install git
+sudo apt install python3
+sudo apt install python2
+sudo apt install nano
+sudo apt install mc
+sudo apt install sl
+sl
+sudo apt install ruby
+echo "все записаные пакеты были обнавлены!/all recorded packages have been updated!"
---
+++ termux.sh
@@ -0,0 +1,12 @@
+#!bin/bash 
+
+pkg install git 
+pkg install python
+pkg install python3
+pkg install python2
+pkg install sl 
+sl 
+
+pkg install nano 
+pkg install mc 
+echo "все записаные пакеты были обнавлены!/all recorded packages have been updated!"
