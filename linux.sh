6a3604e77f07803be8103aa223749471ba6f6cf7
NiTrOn43 <101572497+NiTrOn43@users.noreply.github.com>
2022-05-07 13:24:08 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -5,7 +5,10 @@ sudo apt install python3
 sudo apt install python2
 sudo apt install nano
 sudo apt install mc
-sudo apt install sl
-sl
 sudo apt install ruby
+sudo apt install aptitude
+sudo apt install sl
+sudo passwd
+su
+clear
 echo "все записаные пакеты были обнавлены!/all recorded packages have been updated!"
